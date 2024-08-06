74abada2de1e2871024c2b976da83036874bfe53
Jacobo <ticiochatbo@gmail.com>
2024-08-06 12:10:08 -0500

    inital commit

---
+++ app.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_socketio import SocketIO, send
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+socketio = SocketIO(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    submit = SubmitField('Login')
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    submit = SubmitField('Sign Up')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password_hash, password):
+            login_user(user)
+            return redirect(url_for('chat'))
+        else:
+            return 'Invalid username or password', 401
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        if User.query.filter_by(username=username).first():
+            return 'Username already exists', 400
+        password_hash = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, password_hash=password_hash)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/')
+@login_required
+def chat():
+    return render_template('chat.html')
+
+""""
+@app.route('/get_conversations')
+@login_required
+def get_conversations():
+    user_id = current_user.id
+    conversations = Conversation.query.filter_by(user_id=user_id).all()
+    conversations_list = [conv.conversation for conv in conversations]
+    return jsonify(conversations=conversations_list)
+"""
+
+@socketio.on('message')
+def handle_message(message):
+    print(f'Received message: {message}')
+    response = f'You said: {message}'
+    send(response)
+    
+    """
+    # Save conversation to the database
+    if current_user.is_authenticated:
+        new_conversation = Conversation(user_id=current_user.id, conversation=message)
+        db.session.add(new_conversation)
+        db.session.commit()
+    """
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    socketio.run(app, debug=True)
