7df75307d68b84328e75f323acfc7addfb3dd3d5
Jacobo <ticiochatbo@gmail.com>
2024-09-09 15:25:56 -0500

    it was the K

--- app.py
+++ app.py
@@ -139,4 +139,4 @@ def handle_research_message(message):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app)
+    socketio.run(app, allow_unsafe_werkzeug= True)
